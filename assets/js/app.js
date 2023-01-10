56719741126385e851a61ab53f84a4dc712fb6c4
AkiraPhat18 <phatnguyen19111997@gmail.com>
2023-01-10 11:13:31 +0700

    new commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,37 +1,30 @@
-var imgFeature = document.querySelector('.img-feature')
-var listImage = document.querySelectorAll('.list-image img')
-var prevBtn = document.querySelector('.prev')
-var nextBtn = document.querySelector('.next')
+let slideIndex = 1;
+showSlides(slideIndex);
 
-var currentIndex = 0;
-
-function updateImagebyIndex(index) {
-    //remove active class
-    document.querySelectorAll('.list-image div').forEach(item => {
-        item.classList.remove('active')
-    })
-
-    currentIndex = index 
-    imgFeature.src =  listImage[index].getAttribute('src')
-    listImage(index).parentElement.classList.add('active')
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-listImage.forEach((imgElement, index) => {
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-    imgElement.addEventListener('click', e => {
-        updateImagebyIndex(index)
-       
-    })
-})
-
-prevBtn.addEventListener('click', e=>{
-    if(currentIndex == 0) {
-        currentIndex = listImage.length - 1
-    }else{
-        currentIndex--
-    }
-
-    updateImagebyIndex(currentIndex)
-})
-
-updateImagebyIndex(0)
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
